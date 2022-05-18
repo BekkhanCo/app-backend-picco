7d47d3a12b680752dd547aaeed241414d17923e4
imBOB <bekzodvaliev1@gmail.com>
2022-05-18 21:54:40 +0500

    maven project created and necessary libs added

---
+++ src/main/java/com/example/appyadropicco/AppYadroPiccoApplication.java
@@ -0,0 +1,13 @@
+package com.example.appyadropicco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppYadroPiccoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppYadroPiccoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appyadropicco/AppYadroPiccoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appyadropicco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppYadroPiccoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
